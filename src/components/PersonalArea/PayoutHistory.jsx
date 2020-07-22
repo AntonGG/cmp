52d8ad923070505fa31b44df26c72695b7195093
Anton <byghoster@gmail.com>
2020-07-22 20:25:54 +0500

    Верстка таблицы

--- src/components/PersonalArea/PayoutHistory.jsx
+++ src/components/PersonalArea/PayoutHistory.jsx
@@ -4,10 +4,28 @@ const PayoutHistory = () => {
   return (
     <div className="payout-history">
       <div className="payout-history__menu">
-        <p>История выплат</p>
-        <button>Скачать отчет</button>
+        <p className="payout-history__title">История выплат</p>
+        <button className="payout-history__download-button">
+          Скачать отчет
+        </button>
       </div>
-      
+      <table className="payout-history__table">
+        <tr>
+          <td>22.01.2020</td>
+          <td>В обработке</td>
+          <td>135 токенов</td>
+        </tr>
+        <tr className="payout-history__tr-td__gray">
+          <td>22.01.2020</td>
+          <td>В обработке</td>
+          <td>135 токенов</td>
+        </tr>
+        <tr>
+          <td>22.01.2020</td>
+          <td>В обработке</td>
+          <td>135 токенов</td>
+        </tr>
+      </table>
     </div>
   );
 };
