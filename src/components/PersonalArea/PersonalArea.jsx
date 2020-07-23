2b341dcf99219a85ab1657153e0d4779394982a9
Anton <byghoster@gmail.com>
2020-07-23 19:14:22 +0500

    refactoring

--- src/components/PersonalArea/PersonalArea.jsx
+++ src/components/PersonalArea/PersonalArea.jsx
@@ -4,14 +4,24 @@ import Menu from "./Menu";
 import PayoutHistory from "./PayoutHistory";
 import WithdrawFunds from "./CashOut";
 import "../../sass/personalArea/personalArea.sass";
+import Partners from "./Partners";
+import LastCompletedTasks from "./LastCompletedTasks";
 const PersonalArea = () => {
   return (
     <div className="personal-area">
       <p className="title">Личный кабинет</p>
       <Menu type={true} />
-      <Balance />
-      <WithdrawFunds />
-      <PayoutHistory />
+      <div className="personal-area__body">
+        <div>
+          <Balance />
+          <WithdrawFunds />
+          <PayoutHistory />
+        </div>
+        <div>
+          <Partners />
+          <LastCompletedTasks />
+        </div>
+      </div>
     </div>
   );
 };
