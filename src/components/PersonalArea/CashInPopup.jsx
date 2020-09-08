9ec53811556286821a73d5961ac7bd2ee9d40c82
Anton <byghoster@gmail.com>
2020-09-08 23:36:44 +0500

    language

--- src/components/PersonalArea/CashInPopup.jsx
+++ src/components/PersonalArea/CashInPopup.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import "../../sass/cashInPopup.sass";
 import copyToClipboard from "../../utils/copyToClipboard";
 
 const CashInPopup = ({ wallet, isPopup, setIsPopupFalse }) => {
+  const { t } = useTranslation();
+
   useEffect(() => {
     const handleEsc = (event) => {
       if (event.keyCode === 27) {
@@ -24,7 +27,7 @@ const CashInPopup = ({ wallet, isPopup, setIsPopupFalse }) => {
     <div className="cash-in-popup">
       <div className="cash-in-popup__container">
         <div className="cash-in-popup__title">
-          <p>Пополнить {wallet.currency}</p>
+          <p>{`${t("cash_in_popup__cash_in")} ${wallet.currency}`}</p>
 
           <div onClick={() => setIsPopupFalse()}>
             <div className="cl-btn-7"></div>
@@ -32,24 +35,24 @@ const CashInPopup = ({ wallet, isPopup, setIsPopupFalse }) => {
         </div>
         <div className="cash-in-popup__input">
           <input
-            placeholder="Ваш адрес"
+            placeholder={t("cash_in_popup__your_address")}
             defaultValue={wallet ? wallet.address : ""}
           />
           <div
             onClick={() => copyToClipboard(wallet ? wallet.address : "")}
             className="cash-in-popup__copy-button"
           >
-            <p>Копировать</p>
+            <p>{t("cash_in_popup__copy")}</p>
           </div>
         </div>
-        <p>
-          Для пополнения кошелька отправляйте на этот адрес только{" "}
-          {wallet.currency}. Отправка монеты или токена, кроме {wallet.currency}{" "}
-          по этому адресу может привести к потере средств.
-        </p>
+        <p>{t("cash_in_popup__info", { currency: wallet.currency })}</p>
       </div>
     </div>
   );
 };
 
 export default CashInPopup;
+
+// Для пополнения кошелька отправляйте на этот адрес только{" "}
+// {wallet.currency}. Отправка монеты или токена, кроме {wallet.currency}{" "}
+// по этому адресу может привести к потере средств.
